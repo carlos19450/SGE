49fe6f20a367bc5963b0adba5dc4e36c61f24b73
Carlos <infocarlosexpuig@gmail.com>
2022-11-23 17:35:01 +0100

    Ejercicio python

--- Tema1/EjerciciosBasicos/2/Ejercicio11.py
+++ Tema1/EjerciciosBasicos/2/Ejercicio11.py
@@ -1,3 +1,4 @@
 nombreProducto = input("Introduce el nombre del producto: ")
-precioProducto = int(input("Introduce el precio del producto: "))
+precioProducto = float(input("Introduce el precio del producto: "))
 unidadesProducto = int(input("Introduce el numero de unidades: "))
+print('{producto}: {unidades:3d} unidades x {precio:9.2f}€ = {total:11.2f}€'.format(producto = nombreProducto, unidades = unidadesProducto, precio = precioProducto, total = unidadesProducto * precioProducto))
